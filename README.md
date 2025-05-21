3789f4586b88a8ec8446726ab96525a78e4ee9ae
cbodett <courtney.bodett@gmail.com>
2025-05-20 22:09:48 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# EsPreSso
+PowerShell module for generating unique and memorable passphrases
+
+## Installation
+To install EsPreSso
+```Powershell
+PS> # to do
+
+```
+
+
+
+## Made With Sampler
+This project was made using [Sampler Module](https://github.com/gaelcolas/Sampler)
+See their [video presentation](https://youtu.be/tAUCWo88io4?si=jq0f7omwll1PtUsN) from the PowerShell summit for a great demonstration.
---
+++ source/Public/Start-KeepAwake.ps1
@@ -0,0 +1,75 @@
+function Start-KeepAwake {
+    <#
+    .SYNOPSIS
+    This function attempts to keep the computer awake by sending a key-press every 60 seconds (by default)
+    .DESCRIPTION
+    Using the WScript namespace to send keys this function will send a key combination of shift+F15 every 60 seconds by default to keep the computer awake and preven the screensaver
+    from activating.
+    .PARAMETER Minutes
+    Number of minutes to run the keep awake
+    .PARAMETER Hours
+    Number of hours to run the keep awake
+    .PARAMETER Until
+    A date time representing when to stop running the keep awake. Accepts common datetime formats
+    .PARAMETER Interval
+    The interval for how often a key press is sent. Default is 60 seconds and this should be fine for most scenarios.
+    .EXAMPLE
+    PS> Start-KeepAwake
+
+    When ran with no parameters the function will attempt to keep the computer away indefinitely until cancelled.
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'Manual')]
+    [Alias("nosleep","ka")]
+    Param(
+        [Parameter(Position = 1, ParameterSetName = 'Manual')]
+        [Alias("m")]
+        [Int32]$Minutes,
+        [Parameter(Position = 0, ParameterSetName = 'Manual')]
+        [Alias("h")]
+        [Int32]$Hours,
+        [Parameter(ParameterSetName = 'Until')]
+        [Alias("u")]
+        [DateTime]$Until,
+        [ValidateRange(1,86400)]
+        [Int32]$Interval = 60
+    )
+
+    $TSParams = @{}
+    switch ($PSBoundParameters.Keys) {
+        'Minutes' {
+            $TSParams.Add('Minutes', $Minutes)
+            Write-Verbose "Adding $Minutes minutes of duration"
+        }
+        'Hours' {
+            $TSParams.Add('Hours', $Hours)
+            Write-Verbose "Adding $Hours hours of duration"
+        }
+        'Until' {
+            Write-Verbose "Stopping time provided of: $($Until.ToShortTimeString())"
+            $UntilDuration = ($Until - (Get-Date)).TotalMinutes
+            If ($UntilDuration -lt 1) {
+                $UntilDuration = 1
+            }
+            Write-Verbose "Adding $UntilDuration minutes of duration"
+            $TSParams.Add('Minutes', $UntilDuration)
+        }
+    }
+    if (-not $TSParams.Count) {
+        Write-Verbose "Defaulting to indefinite runtime"
+        $Duration = $true
+    } else {
+        $Duration = (New-TimeSpan @TSParams).TotalMinutes
+        Write-Verbose "Total duration is $Duration minutes"
+    }
+
+    $WShell = New-Object -ComObject WScript.Shell
+    Write-Verbose "Keeping computer awake by sending 'Shift + F15' every $Interval seconds"
+    while ($RunTime -le $Duration) {
+        if ($Duration.GetType().Name -ne "Boolean") {
+            Write-Verbose $('{0:n2} minutes remaining' -f ($Duration - $Runtime))
+            $Runtime += $($Interval/60)
+        }
+        $WShell.SendKeys('+{F15}')
+        Start-Sleep -seconds $Interval
+    }
+}
