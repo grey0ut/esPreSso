b427e8e2f203936bf983b0d1cd7aa4ddc7a49514
cbodett <courtney.bodett@gmail.com>
2025-05-24 12:40:44 -0700

    initial tests of scheduled task method completed

--- source/Public/Get-KeepAwake.ps1
+++ source/Public/Get-KeepAwake.ps1
@@ -1,13 +1,25 @@
 function Get-KeepAwake {
     <#
     .SYNOPSIS
-    Checks the registered scheduled jobs for the esPreSso job that runs Start-KeepAwake
+    Checks the registered scheduled tasks for the esPreSso job that runs Start-KeepAwake
     .DESCRIPTION
-    Checks the registered scheduled jobs for the esPreSso job that runs Start-KeepAwake and return an object representing its settings
+    Checks the registered scheduled tasks for the esPreSso job that runs Start-KeepAwake and return an object representing its settings
+    .EXAMPLE
+    PS> Get-KeepAwake
+
+    TaskPath                                       TaskName                          State
+    --------                                       --------                          -----
+    \Microsoft\Windows\PowerShell\ScheduledJobs\   esPreSso                          Ready
 
     #>
     [CmdletBinding()]
-    param (
+    param ()
 
-    )
+    $TaskName = "esPreSso"
+    $TaskPath = "\Microsoft\Windows\PowerShell\ScheduledJobs\"
+    try {
+        Get-ScheduledTask -TaskName $TaskName -TaskPath $TaskPath -ErrorAction Stop
+    } catch {
+        Write-Warning "esPreSso scheduled task not found"
+    }
 }
--- source/Public/Remove-KeepAwake.ps1
+++ source/Public/Remove-KeepAwake.ps1
@@ -1,10 +1,19 @@
 function Remove-KeepAwake {
     <#
-
+    .SYNOPSIS
+    Removes the esPreSso scheduled task from Task Scheduler
+    .DESCRIPTION
+    Unregisters the scheduled task created by Register-KeepAwake. There is no output.
+    .EXAMPLE
+    PS> Remove-KeepAwake
     #>
     #Requires -RunAsAdministrator
     [CmdletBinding()]
-    param (
+    param ()
 
-    )
+    $ScheduledTask = Get-KeepAwake
+    if ($ScheduledTask) {
+        Write-Verbose "Removing scheduled task named $($ScheduledTask.TaskName) in $($ScheduledTask.TaskPath)"
+        Unregister-ScheduledTask -InputObject $ScheduledTask -Confirm:$false
+    }
 }
